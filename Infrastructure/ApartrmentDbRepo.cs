0c929dce5628829a77f18d29f9e07275e2a71754
GitKavindu <bakh99810@gmail.com>
2025-06-23 19:34:39 +0530

    updated seats to return in order

--- Infrastructure/ApartrmentDbRepo.cs
+++ Infrastructure/ApartrmentDbRepo.cs
@@ -96,7 +96,10 @@ public class ApartmentDbRepo:IApartmentDbRepo
           foreach(var i in apartment)
           {
             IEnumerable<SeatModel> seats=await con.QueryAsync<SeatModel>(
-            $"SELECT is_left AS isLeft,row_no AS rowNo,seq_no AS seqNo,apartment_id AS apartmentId FROM seat WHERE apartment_id={i.Apartment_id}"
+            @$"SELECT is_left AS isLeft,row_no AS rowNo,seq_no AS seqNo,apartment_id AS apartmentId 
+                FROM seat 
+                WHERE apartment_id={i.Apartment_id}
+                ORDER BY row_no,is_left DESC,seq_no"
             , commandType: CommandType.Text);
 
             i.seatModel=seats.ToArray();
